6f7fca22f713ced63170bd3b726096ad1ad9e3ff
Uzair Aslam <mruzairaslam1@gmail.com>
2025-05-02 15:04:36 +0500

    error handling and optimization

--- routes/category.js
+++ routes/category.js
@@ -4,11 +4,12 @@ const router  = express.Router();
 const {pool} = require("../config/db")
 
 router.post("/add", async (req,res)=>{
-    const {name,description} = req.body;
+    try{
+        const {name,description} = req.body;
 
     if(!name){
         console.log("Category name is required.");
-        res.status(400).json({message: "Name, price and category is required"})
+        return res.status(400).json({message: "Name, price and category is required"})
     }
 
     const query = {
@@ -17,23 +18,27 @@ router.post("/add", async (req,res)=>{
     };
 
     const queryRes = await pool.query(query);
-
-    if(queryRes.rows.length === 0){
-        console.log(`Error adding category.`);
-        res.status(500).json({message: "Error adding category"});
-    }
+    const newCategory = queryRes.rows[0];
 
     console.log(`Added category:${queryRes.rows[0]}`);
-    res.status(200).json({message: "Category successfully added"})
+    return res.status(200).json({message: "Category successfully added",
+        category: newCategory
+    })
+    }
+    catch(error){
+        console.log(`Added category:${queryRes.rows[0]}`);
+        return res.status(500).json({message: "Internal Server Error",error: error.message})
+    }
 });
 
 router.put("/update/:id",async (req,res)=>{
-    const recordId = req.params.id;  //WORKNING WITH HEADERS??
+    try{
+        const recordId = req.params.id;  //WORKNING WITH HEADERS??
     const {name,description} = req.body;
 
     if(!name){
         console.log("Category name is required.");
-        res.status(400).json({message: "Category is required"})
+        return res.status(400).json({message: "Category is required"})
     }
 
     const query = {
@@ -45,14 +50,22 @@ router.put("/update/:id",async (req,res)=>{
     };
 
     const queryRes = await pool.query(query);
+    const updatedCategory = queryRes.rows[0];
 
     if(queryRes.rows.length === 0){
-        console.log(`Error updating category.`);
-        res.status(500).json({message: "Error updating category"});
+        console.log(`Category not found.`);
+        return res.status(500).json({message: "Category not found",
+            category: updatedCategory
+        });
     }
 
     console.log(`updated category name:${queryRes.rows[0].name}`);
-    res.status(200).json({message: "Category successfully updated"})
-})
+    return res.status(200).json({message: "Category successfully updated",category: updatedCategory})
+    }
+    catch(error){
+        console.log("Internal Server Error",error);
+        return res.status(500).json({message:"Internal server error",error: error.message})
+    }
+});
 
 module.exports = router;
